3af51debd9079dcee62663b7dab8fe617a8800a6
minung.han <hmu332233@gmail.com>
2019-08-09 00:12:22 +0900

    feat: view 및 request 로직 추가

--- code/request.js
+++ code/request.js
@@ -1,6 +1,16 @@
 module.exports.function = function request () {
   const http = require('http');
+  const console = require('console');
+  const data = {
+    userRequest: {
+      user: {
+        id: 'bixby_user'
+      },
+      utterance: '진수당'
+    }
+  }
+  const res = http.postUrl('', data, { passAsJson: true });
   return {
-    message: 'test'
-  };
+    message: JSON.parse(res).template.outputs[0].simpleText.text
+  }
 }
